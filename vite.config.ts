3c6e9b5b265d901f524ea516b5a78eed0532c062
Mirko Lenz <mirko@mirkolenz.com>
2023-10-04 20:04:14 +0200

    fix: use proper entrypoint for d.ts files

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ const external = (id: string) =>
   !id.startsWith("\0") && !id.startsWith(".") && !id.startsWith("/");
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [dts({ entryRoot: "./src" })],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
+      name: " arg-services",
       fileName: "index",
       formats: ["es"],
     },
