2bab2926563411377cf425c2b1179b39592f8893
Mirko Lenz <mirko@mirkolenz.com>
2024-04-04 11:22:14 +0200

    feat!: update build process and dependencies
    BREAKING CHANGE: We switched from `@bufbuild/connect` to `@connectrpc/connect`, so please update your imports accordingly.

--- flake.nix
+++ flake.nix
@@ -3,47 +3,36 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    npmlock2nix = {
-      url = "github:nix-community/npmlock2nix";
-      flake = false;
-    };
   };
-  outputs = inputs @ {
-    nixpkgs,
-    flake-parts,
-    systems,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = {
-        pkgs,
-        lib,
-        ...
-      }: let
-        npmlock2nix = import inputs.npmlock2nix {inherit pkgs;};
-      in {
-        packages = {
-          releaseEnv = pkgs.buildEnv {
-            name = "release-env";
-            paths = with pkgs; [nodejs];
+      perSystem =
+        { pkgs, lib, ... }:
+        {
+          packages = {
+            releaseEnv = pkgs.buildEnv {
+              name = "release-env";
+              paths = with pkgs; [ nodejs ];
+            };
+            bufGenerate = pkgs.writeShellApplication {
+              name = "buf-generate";
+              text = ''
+                ${lib.getExe pkgs.buf} mod update
+                ${lib.getExe pkgs.buf} generate --include-imports buf.build/recap/arg-services
+              '';
+            };
           };
-          bufGenerate = pkgs.writeShellApplication {
-            name = "buf-generate";
-            text = ''
-              export PATH="${lib.getBin npmlock2nix.v2.node_modules {
-                src = ./.;
-                nodejs = pkgs.nodejs;
-              }}/bin:$PATH"
-              ${lib.getExe pkgs.buf} mod update
-              ${lib.getExe pkgs.buf} generate --include-imports buf.build/recap/arg-services
-            '';
+          devShells.default = pkgs.mkShell {
+            shellHook = "npm install";
+            packages = with pkgs; [ nodejs ];
           };
         };
-        devShells.default = pkgs.mkShell {
-          shellHook = "npm install";
-          packages = with pkgs; [nodejs];
-        };
-      };
     };
 }
